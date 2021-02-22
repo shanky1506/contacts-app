f07966808c99ec6680e97934ec29939cc8b7733e
shanky1506 <shashankgunda68@outlook.com>
2021-02-22 21:15:42 +0530

    google authentication working

--- server/index.js
+++ server/index.js
@@ -1,30 +1,55 @@
 const express = require('express')
 const cors = require('cors');
-const mongoose = require('mongoose');
+const connectDB = require('./config/db');
+const morgan = require('morgan')
+const passport = require('passport')
+const session = require('express-session')
 require('dotenv').config()
 
+require('./config/passport')(passport)
+
+
 const app = express()
+app.use(morgan('tiny'))
+
+//Session middleware 
+
+app.use(session({
+    secret :'keyboard cat',
+    resave:false,
+    saveUninitialized:false,
+}))
+
+// Passport MiddleWare
+app.use(passport.initialize())
+app.use(passport.session())
+
+//ROUTES
+app.use('/',require('./routes/index.route'))
+app.use('/auth',require('./routes/auth.route'))
+
+
 const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
 
-const uri = process.env.ATLAS_URI
-
-mongoose.connect(uri, {
-    useNewUrlParser : true,
-    useCreateIndex : true,
-    useUnifiedTopology: true
-});
-
-const connection = mongoose.connection;
-
-connection.once('open',()=>{
-    console.log("MongoDB database Connection established")
-})
-
+connectDB()
 
 app.listen(port,() => {
     console.log(`Server is running on port : ${port}`);
 })
 
+/* 
+// mongoose.connect(uri, {
+//     useNewUrlParser : true,
+//     useCreateIndex : true,
+//     useUnifiedTopology: true
+// });
+
+// const connection = mongoose.connection;
+
+// connection.once('open',()=>{
+//     console.log("MongoDB database Connection established")
+// })
+*/
