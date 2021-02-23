3a103b7d19bcb2109f1151d3394fcdedea649c11
shanky1506 <shashankgunda68@outlook.com>
2021-02-24 03:42:02 +0530

    cors issue solved

--- server/index.js
+++ server/index.js
@@ -11,7 +11,8 @@ require('./config/passport')(passport)
 
 const app = express()
 app.use(morgan('tiny'))
-
+app.use(cors())
+app.use(express.json())
 //Session middleware 
 
 app.use(session({
@@ -31,25 +32,10 @@ app.use('/auth',require('./routes/auth.route'))
 
 const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
+
 
 connectDB()
 
 app.listen(port,() => {
     console.log(`Server is running on port : ${port}`);
 })
-
-/* 
-// mongoose.connect(uri, {
-//     useNewUrlParser : true,
-//     useCreateIndex : true,
-//     useUnifiedTopology: true
-// });
-
-// const connection = mongoose.connection;
-
-// connection.once('open',()=>{
-//     console.log("MongoDB database Connection established")
-// })
-*/
