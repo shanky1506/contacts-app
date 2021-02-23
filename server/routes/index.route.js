7f386a863444b610ad31c465918d63c9fcfb04a4
shanky1506 <shashankgunda68@outlook.com>
2021-02-24 03:43:14 +0530

    server API endpoints for contacts added

--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -1,14 +1,46 @@
-const express = require('express')
+const express = require('express');
+const { find } = require('../models/User.model');
 const router = express.Router()
+const User = require('../models/User.model');
 
 // @desc Login 
 // @ route GET / 
 router.get('/',(req,res)=>{
-    res.send('Login')
+    console.log({homereq : req})
+    res.json({
+        Login : "Logged in"
+    })
 })
 // @desc 
 router.get('/user',(req,res)=>{
-    res.send('USER')
+    try{
+        console.log({ userreq: req.user })
+        res.json({
+            hello: "Hi",
+            user: req.user
+        })
+    } catch(e){
+        res.status(400).json({message : e})
+    }
 })
 
+// @desc getContacts 
+// @route GET /contacts/:id
+router.get('/contacts/:id',async (req,res)=>{
+    try{
+        const id = req.params.id;
+        let contacts;
+        const user = await User.findById(id)
+        contacts = user.contacts;
+        if (contacts.length > 0) {
+            res.status(200).json(contacts)
+        } else {
+            res.status(201).json({
+                message: " This account has no contacts"
+            })
+        }
+    } catch(e){
+        res.status(400).json(e)
+    }
+})
 module.exports = router
